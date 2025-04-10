7a781395f074afa26c2ffaa767f75466ad443e5d
libyyyyyy <libyfemboy@gmail.com>
2025-04-10 14:39:43 -0400

    work

--- saltycubes/script.js
+++ saltycubes/script.js
@@ -6,3 +6,19 @@ function changeBackground(imageSrc) {
         div.classList.add('transparent');
     });
 }
+function SecureMode() {
+    var b = document.getElementById("securemode");
+    b.onclick = function() {
+        win = window.open();
+        win.document.title = "Salty Cube's Revamp"
+        win.document.body.style.margin = "0%";
+        win.document.body.style.height = "100%";
+        var iframe = win.document.createElement("iframe");
+        iframe.style.border = "none";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+        iframe.style.margin = "0";
+        iframe.src = "https://libyyyyyy.github.io/saltycubes"
+        win.document.body.appendChild(iframe);
+    }
+}
